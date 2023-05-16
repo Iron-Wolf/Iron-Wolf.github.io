0f014ba15de6a792b397cb4cfecad0924d998665
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2023-05-17 01:54:41 +0200

    basic listing

--- painting/README.md
+++ painting/README.md
@@ -3,10 +3,10 @@
 ## Paint image naming
 Paint files (in paint-img folder) are named with this convention :  
 ```
-<type>-<name>-<owned>.png
+{b|l|s|d|c|t}-<name>[-n].png
 ```
 - type : **B**ase, **L**ayer, **S**hade, **D**ry, **C**ontrast, **T**echnical
 - name : initials of the painting's name 
-- owned : tell that I have the paint (y) or not (n)
+- owned : tell that I have the paint (nothing) or not ("-n" is set)
   - allow me to list what i own
-  - and as an image database for the website
+  - used as an image database for the website
--- painting/js/main.js
+++ painting/js/main.js
@@ -1,10 +1,18 @@
 var divColorGuide = document.getElementById("colorGuide");
+var resourcesPath = "resources/paint-img/"
+var imgSize = "100px"
 
 fetch("./resources/fig-guide/drg-grunt.json")
   .then(response => { return response.json(); })
   .then(data => {
+    // loop on each JSON objects
     data.forEach(element => {
       divColorGuide.innerHTML += element.section
+      // loop on paint IDs (to retrieve the images)
+      element.paints.forEach(paint => {
+        var fullPathImg = `${resourcesPath}${paint}.png`
+        divColorGuide.innerHTML += `<img src=${fullPathImg} title=${paint} width=${imgSize} height=${imgSize}/>`
+      })
       divColorGuide.innerHTML += "<br/>"
     });
   });
