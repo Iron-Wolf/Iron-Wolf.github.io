f1311183d3ce5274f1f8277bce955a7bed606433
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2023-05-17 19:39:57 +0200

    Painting : small fixes

--- painting/js/main.js
+++ painting/js/main.js
@@ -1,20 +1,20 @@
 var divColorRef = document.getElementById("colorRef");
 var divColorGuide = document.getElementById("colorGuide");
-var pathImg = "resources/color-ref/"
-var pathGuide = "resources/color-guide/"
+var pathImg = "./resources/color-ref"
+var pathGuide = "./resources/color-guide"
 var imgSize = "100px"
 
-fetch(`./${pathImg}/all-ref.json`)
+fetch(`${pathImg}/all-ref.json`)
   .then(response => { return response.json(); })
   .then(data => {
     // loop on each string
     data.forEach(paintName => {
-      var fullPathImg = `${pathImg}${paintName}.png`
-      divColorGuide.innerHTML += `<img src=${fullPathImg} title=${paint} width=${imgSize} height=${imgSize}/>`
+      var fullPathImg = `${pathImg}/${paintName}.png`
+      divColorGuide.innerHTML += `<img src=${fullPathImg} title=${paintName} width=${imgSize} height=${imgSize}/>`
     });
   });
 
-fetch(`./${pathGuide}/drg-grunt.json`)
+fetch(`${pathGuide}/drg-grunt.json`)
   .then(response => { return response.json(); })
   .then(data => {
     // loop on each JSON objects
@@ -22,7 +22,7 @@ fetch(`./${pathGuide}/drg-grunt.json`)
       divColorGuide.innerHTML += element.section
       // loop on paint IDs (to retrieve the images)
       element.paints.forEach(paintName => {
-        var fullPathImg = `${pathImg}${paintName}.png`
+        var fullPathImg = `${pathImg}/${paintName}.png`
         divColorGuide.innerHTML += `<img src=${fullPathImg} title=${paintName} width=${imgSize} height=${imgSize}/>`
       })
       divColorGuide.innerHTML += "<br/>"
