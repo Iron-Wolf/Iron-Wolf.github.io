59bd60b8f950c079666a3f310917aad5df01cf85
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2023-05-17 23:54:58 +0200

    Painting : add SVG

--- painting/js/main.js
+++ painting/js/main.js
@@ -4,16 +4,26 @@ var pathImg = "./resources/color-ref"
 var pathGuide = "./resources/color-guide"
 var imgSize = "100px"
 
+// +-----------------+
+// | Color Reference |
+// +-----------------+
 fetch(`${pathImg}/all-ref.json`)
   .then(response => { return response.json(); })
   .then(data => {
     // loop on each string
     data.forEach(paintName => {
       var fullPathImg = `${pathImg}/${paintName}.svg`
-      divColorRef.innerHTML += `<img src=${fullPathImg} title=${paintName} width=${imgSize} height=${imgSize}/>`
+      divColorRef.innerHTML += 
+        `<figure>
+          <img src=${fullPathImg} width=${imgSize} height=${imgSize}/>
+          <figcaption>${paintName}</figcaption>
+        </figure>`
     });
   });
 
+// +-------------+
+// | Color Guide |
+// +-------------+
 fetch(`${pathGuide}/drg-grunt.json`)
   .then(response => { return response.json(); })
   .then(data => {
@@ -22,7 +32,7 @@ fetch(`${pathGuide}/drg-grunt.json`)
       divColorGuide.innerHTML += element.section
       // loop on paint IDs (to retrieve the images)
       element.paints.forEach(paintName => {
-        var fullPathImg = `${pathImg}/${paintName}.png`
+        var fullPathImg = `${pathImg}/${paintName}.svg`
         divColorGuide.innerHTML += `<img src=${fullPathImg} title=${paintName} width=${imgSize} height=${imgSize}/>`
       })
       divColorGuide.innerHTML += "<br/>"
